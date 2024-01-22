4d255f31a62ad9c0b9fb4ec8612e7f4ac5a5f795
gowthamReddy <gowthamreddy.6991@gmail.com>
2024-01-22 16:57:09 +0530

    this is flask application

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Cosco Good Evening this is Gowtham'
+    return 'Hello Cisco Good Evening this is Gowtham'
 
 if __name__ == '__main__':
     app.run()
